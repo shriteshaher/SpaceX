617ffc6b10068c87f71bb38659192cabe558dd5e
Shritesh Aher <shriteshaher@gmail.com>
2022-03-28 18:35:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { CallApiService } from './call-api.service';
+declare var toggle:any;
+declare var toggle2:any;
+declare var toggle3:any
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SpaceX';
+  res1:any=[]
+  year=""
+  landlaunch=false
+  landsucces=false
+  constructor(private callapi:CallApiService){
+
+  }
+
+  /* Get Year */
+  getYear=(btn:string)=>{
+       this.year=btn
+  }
+
+  /* Get Land Launch */
+  getLandLaunch=(launch:boolean)=>{
+    this.landlaunch=launch
+  }
+  
+  /* Get Land Succes */
+ getLandSuccess=(success:boolean)=>{
+    this.landsucces=success
+    this.Filter(this.year,this.landlaunch,this.landsucces)
+  }
+  
+  Filter(year:string,LandLaunch:boolean,LandSuccess:boolean){
+     
+     if(!LandLaunch && LandSuccess){
+      console.log(year,LandLaunch,LandSuccess)
+           this.callapi.getApiLandLaunch(LandSuccess).subscribe(res=>{
+                 this.res1=res
+                 console.log(this.res1['land_success'])
+           })
+
+     }
+
+     else if(year!='2014' && (LandLaunch && LandSuccess)){
+      console.log(year,LandLaunch,LandSuccess)
+      this.callapi.getApiLandLaunchLandSuccess(LandLaunch,LandSuccess).subscribe(res=>{
+            this.res1=res
+            console.log(this.res1)
+      })
+    }
+    else if(year=='2014' && (LandLaunch && LandSuccess)){
+      console.log(year,LandLaunch,LandSuccess)
+        this.callapi.getApiLandLaunchLandSuccessWithYear(year,LandLaunch,LandSuccess).subscribe(res=>{
+          this.res1=res
+          console.log(this.res1['land_success'])
+        })  
+      }
+     
+}
+  ngOnInit(){
+     new toggle()
+     new toggle2()
+     new toggle3()
+     this.callapi.getApi().subscribe(
+       res=>{
+         this.res1=res
+         console.log(this.res1)
+       }
+     )
+     
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatNativeDateModule} from '@angular/material/core';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
